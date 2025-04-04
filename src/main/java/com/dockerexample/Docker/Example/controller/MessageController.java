e0a6b04d970e543bc4ba6eb0d6aece6ea4a02e70
tubelig-ht <69118538+tubelig-ht@users.noreply.github.com>
2025-04-05 02:04:48 +0530

    Update MessageController.java

--- src/main/java/com/dockerexample/Docker/Example/controller/MessageController.java
+++ src/main/java/com/dockerexample/Docker/Example/controller/MessageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessageController{
 
-    @GetMapping("/friend/message")
+    @GetMapping("/message")
     public String sayHello(){
         return "Hello World!";
     }
